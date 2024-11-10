1c645a5cb71bf9f5e09cf6999dbedb55efcc8442
moonstruck19 <lamborghininguyn@gmail.com>
2024-11-10 16:49:17 +0700

    update layout server

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
     res.send({status: "Started"})
 })
 
-// Register route
+//================================LOGIN/REGISTER===================================//
 app.post('/register', async (req, res) => {
     const { user_email, user_password } = req.body
     const encryptedPassword = await bcrypt.hash(user_password, 5)
@@ -98,6 +98,8 @@ app.post('/login', async (req, res) => {
     }
 })
 
+//================================TASK MANAGERMENT===================================//
+
 app.post('/addTask', async(req, res) => {
     const { task_name, task_description, task_date_start, task_date_end, task_member, task_status } = req.body
 
@@ -119,109 +121,6 @@ app.post('/addTask', async(req, res) => {
             status: "error",
             message: error.message,
         })
-    }
-})
-
-app.post('/addCrop', async(req, res) => {
-    const { crop_name, crop_date, crop_details } = req.body
-
-    try { 
-        await Crop.create({
-            crop_name: crop_name,
-            crop_date: crop_date,
-            crop_details: crop_details,
-        })
-        res.status(201).send({
-            status: "ok",
-            message: "Crop created successfully"
-        })
-    } catch (error) {
-        res.status(500).send({
-            status: "error",
-            message: error.message,
-        })
-    }
-})
-
-app.post('/addAnimal', async(req, res) => {
-    const {animal_name, animal_date, animal_details, animal_quantity } = req.body
-
-    try {
-        await Animal.create({
-            animal_name: animal_name,
-            animal_date: animal_date, 
-            animal_details: animal_details,
-            animal_quantity: animal_quantity
-        })
-        res.status(201).send({
-            status: "ok",
-            message: "Animal add successfully"
-        })
-    } catch (error) {
-        res.status(500).send({
-            status: "error",
-            message: error.message
-        })
-    }
-})
-
-app.post('/addEmp', async(req, res) => {
-    const { emp_name, emp_age, emp_phone, emp_address, emp_salary } = req.body
-
-    try {
-        await Emp.create({
-            emp_name: emp_name,
-            emp_age: emp_age,
-            emp_phone: emp_phone,
-            emp_address: emp_address,
-            emp_salary: emp_salary,
-        })
-        res.status(201).send({
-            status: "ok",
-            message: "Emp add successfully"
-        })
-    } catch (error) {
-        res.status(500).send({
-            status: "error",
-            message: error.message
-        })
-    }
-})
-app.get('/fetchEmp', async(req, res) => {
-    try {
-        const data = await Emp.find({})
-        res.send({
-            status: "ok",
-            data: data
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-app.get('/fetchCrop', async(req, res) => {
-    try {
-        const data = await Crop.find({})
-        res.send({
-            status: "ok",
-            data: data
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-app.get('/fetchAnimal', async(req, res) => {
-    try {
-        const data = await Animal.find({})
-        res.send({
-            status: "ok",
-            data: data
-        })
-    } catch (error) {
-        console.log(error)
     }
 })
 
@@ -298,7 +197,118 @@ app.put('/editTask', async (req, res) => {
       res.status(500).json({ message: "Internal server error", error: error.message })
     }
   })
-  
+
+//================================CROP MANAGERMENT===================================//
+
+app.post('/addCrop', async(req, res) => {
+    const { crop_name, crop_date, crop_details } = req.body
+
+    try { 
+        await Crop.create({
+            crop_name: crop_name,
+            crop_date: crop_date,
+            crop_details: crop_details,
+        })
+        res.status(201).send({
+            status: "ok",
+            message: "Crop created successfully"
+        })
+    } catch (error) {
+        res.status(500).send({
+            status: "error",
+            message: error.message,
+        })
+    }
+})
+
+app.get('/fetchCrop', async(req, res) => {
+    try {
+        const data = await Crop.find({})
+        res.send({
+            status: "ok",
+            data: data
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+//================================ANIMAL MANAGERMENT===================================//
+
+app.post('/addAnimal', async(req, res) => {
+    const {animal_name, animal_date, animal_details, animal_quantity } = req.body
+
+    try {
+        await Animal.create({
+            animal_name: animal_name,
+            animal_date: animal_date, 
+            animal_details: animal_details,
+            animal_quantity: animal_quantity
+        })
+        res.status(201).send({
+            status: "ok",
+            message: "Animal add successfully"
+        })
+    } catch (error) {
+        res.status(500).send({
+            status: "error",
+            message: error.message
+        })
+    }
+})
+
+app.get('/fetchAnimal', async(req, res) => {
+    try {
+        const data = await Animal.find({})
+        res.send({
+            status: "ok",
+            data: data
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//================================EMPLOYEE MANAGERMENT===================================//
+
+app.post('/addEmp', async(req, res) => {
+    const { emp_name, emp_age, emp_phone, emp_address, emp_salary } = req.body
+
+    try {
+        await Emp.create({
+            emp_name: emp_name,
+            emp_age: emp_age,
+            emp_phone: emp_phone,
+            emp_address: emp_address,
+            emp_salary: emp_salary,
+        })
+        res.status(201).send({
+            status: "ok",
+            message: "Emp add successfully"
+        })
+    } catch (error) {
+        res.status(500).send({
+            status: "error",
+            message: error.message
+        })
+    }
+})
+
+app.get('/fetchEmp', async(req, res) => {
+    try {
+        const data = await Emp.find({})
+        res.send({
+            status: "ok",
+            data: data
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 
 // Start server
 app.listen(5001, () => {
